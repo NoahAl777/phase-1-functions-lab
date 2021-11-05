83e4e2defbdd690065900c7fd4535a17c1b6841a
Noah <crashlive112@gmail.com>
2021-11-05 12:22:29 -0400

    commit tree cc3d3f3964b39fd62dfd1cea0febe3db4211d97b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const hqStreet = 42;
+
+function distanceFromHqInBlocks(pickupLocation) {
+  if (hqStreet > pickupLocation) {
+    return hqStreet - pickupLocation;
+  }
+  else {
+    return pickupLocation - hqStreet;
+  };
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(pickupLocation, dropoffLocation) {
+  if (pickupLocation - dropoffLocation < 0) {
+    return 264 * (pickupLocation - dropoffLocation) * -1;
+  }
+  else {
+    return 264 * (pickupLocation - dropoffLocation);
+  };
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance < 400) {
+    return 0;
+  }
+  else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  }
+  else if (distance <= 2500) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far';
+  }
+}
